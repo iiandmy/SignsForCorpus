d5dd86b4a40540b89cddecaf2ab8b8df17169801
iia.m.y <frezhgrand@gmail.com>
2022-07-22 22:26:37 +0300

    extension for html string decoding + language config

---
+++ SignsForCorpus/Config/Config.swift
@@ -0,0 +1,14 @@
+//
+// Created by IlyaCool on 22.07.22.
+//
+
+import Foundation
+
+struct Config {
+    enum Language: Int {
+        case belorussian = 1
+        case czech = 2
+        case russian = 3
+        case english = 4
+    }
+}
