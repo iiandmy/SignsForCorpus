e20e640d47e18d8efe845f4de041ce20dc8ab399
iia.m.y <frezhgrand@gmail.com>
2022-07-22 16:47:25 +0300

    Initial commit

---
+++ SignsForCorpus/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SignsForCorpus
+//
+//  Created by IlyaCool on 22.07.22.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
